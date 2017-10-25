d7e4a27fc177d5e32c9456d616bedd46e954b24c
RossEastman <rosseastman@LPT00329MAC.local>
2017-10-25 15:18:19 +0100

    took test script outa orbott

--- whatAreYouDoing.app/Contents/Resources/Scripts/orbot.sh
+++ whatAreYouDoing.app/Contents/Resources/Scripts/orbot.sh
@@ -3,7 +3,6 @@
 
 POSTDATA="{\"userId\": \"\",\"taskId\": \"$1\",\"duration\": \"$2\",\"text\": \"$3\",\"date\": \"`date +"%Y/%m/%d"`\"}"
 
-echo "$POSTDATA" >> ~/Desktop/test.txt
 curl  -d "${POSTDATA}" \
 -H "Content-Type: application/json" \
 -H "user_agent: Slackbot, from laptop" \
